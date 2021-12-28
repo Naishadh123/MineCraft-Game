406e413b0dfe08655aeb0cf9394d9c08deba26a3
Naishadh123 <84559186+Naishadh123@users.noreply.github.com>
2021-12-28 14:54:57 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -136,49 +136,49 @@ console.log("c is pressed");
 function up()
 {
 if(player_y>=0)
-(
+{
 player_y=player_y-block_image_height;
 console.log("Block Image Height"+block_image_height);
 console.log("When Up arrow is pressed,X="+player_x+"Y="+player_y);
 canvas.remove(player_object);
 player_update();
-)
+}
 }
 
 function down()
 {
 if(player_y<=450)
-(
+{
 player_y=player_y+block_image_height;
 console.log("Block Image Height"+block_image_height);
 console.log("When Down arrow is pressed,X="+player_x+"Y="+player_y);
 canvas.remove(player_object);
 player_update();
-)
+}
 }
 
 function left()
 {
 if(player_x>=0)
-(
+{
 player_x=player_x-block_image_width;
 console.log("Block Image Width"+block_image_width);
 console.log("When Left arrow is pressed,X="+player_x+"Y="+player_y);
 canvas.remove(player_object);
 player_update();
-)
+}
 }
 
 function right()
 {
 if(player_x<=850)
-(
+{
 player_x=player_x+block_image_width;
 console.log("Block Image Height"+block_image_width);
 console.log("When Right arrow is pressed,X="+player_x+"Y="+player_y);
 canvas.remove(player_object);
 player_update();
-)
+}
 }
 
 
